82f97689f4145f28f84eae4a6df0c9dce2b08309
Jesse Wolgamott <jesse@comalproductions.com>
2017-08-01 14:54:39 -0500

    Add async :thumbs-up:

--- app/async.js
+++ app/async.js
@@ -2,12 +2,14 @@ asyncAnswers = {
   /**
    * Asynchronously returns a value via a promise. Example:
    * async('anyValue').then((result) => { return result === 'anyValue';});
-   * 
+   *
    * @param value - Any value
    * @returns {then: function} A promise like object containing a then property.
    */
   async: function async(value) {
-
+    return {
+      then: (fn) => {fn(value)}
+    }
   },
 
   /**
@@ -15,12 +17,20 @@ asyncAnswers = {
    * You may use jquery, XMLHttpRequest, or fetch.
    * https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
    * https://api.jquery.com/jQuery.ajax/
-   * https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API   * 
-   * 
+   * https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API   *
+   *
    * @param {String} url - a valid url
    * @returns {then: function} A promise like object containing a then property.
    */
   manipulateRemoteData: function manipulateRemoteData(url) {
-
+    return {
+      then: (fn) => {
+        fetch(url)
+        .then( r => r.json() )
+        .then( json => json.people.map( p => p.name))
+        .then( names => names.sort() )
+        .then( fn )
+      }
+    }
   },
 };
